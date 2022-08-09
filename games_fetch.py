bc257349c010a94ff830ad6fb8c813209f9e6fee
Ghzhegosh <altinbaishik@gmail.com>
2022-08-09 22:49:39 +0500

    Made async parsing of list of games. Time took to fetch whole list of games decreased from 2.5 hours to 0.5 minute, but looks like there is a trouble with API that i`m fetching and semaphore doesn`t limit rate of requests so it will work fine. So i`d have to find another way to make everything work fine

--- games_fetch.py
+++ games_fetch.py
@@ -1,60 +1,43 @@
-import json
-import datetime
+import asyncio
+import aiohttp
+import time
 
-import requests
-
-from ratelimit import rate_limited
-
-
-# fetching_link = 'https://www.speedrun.com/api/v1/games'
-# fetching_response = requests.get(fetching_link)
-# response_body = json.loads(fetching_response.text)
-# scheme_of_pages = response_body['data']
-# print(scheme_of_pages[0]['links'][1]['uri'])
-
-# function checks if there is a link to next page with list of games or there is none
+start = time.time()
 
 
-def get_next_page_link(existent_link):
-    fetching_response = requests.get(existent_link)
-    response_body = json.loads(fetching_response.text)
-    scheme_of_pages = response_body['pagination']
-    links_in_scheme = scheme_of_pages['links']
-    amount_of_links_in_scheme = len(scheme_of_pages['links'])
-
-    # далее проверка того что ссылка ведёт на следующую страницу, если их 2 то, то что одна из ссылок является следующей
-    if amount_of_links_in_scheme == 1 and (links_in_scheme[0])['rel'] == 'next':
-        existent_link = (links_in_scheme[0])['uri']
-        return existent_link
-    elif amount_of_links_in_scheme == 2 and (links_in_scheme[1])['rel'] == 'next':
-        existent_link = (links_in_scheme[1])['uri']
-        return existent_link
-    elif amount_of_links_in_scheme == 1 and (links_in_scheme[0])['rel'] == 'prev':
-        return None
-
-
-def fetching_process(existent_link):
-    # links_to_runs = []
+def get_jsons(results):
     list_of_games = open('list_of_games/list_of_games.txt', 'a')
-    while existent_link is not None:
-        fetching_response = requests.get(existent_link)
-        response_body = json.loads(fetching_response.text)
-        scheme_of_body = response_body['data']
-        for game in scheme_of_body:
-            # links_to_runs.append(game['links'][1]['uri'])
+    for result in results:
+        # print(result)
+        json_body = result.json()
+        print(json_body)
+        scheme = json_body['data']
+        for game in scheme:
             list_of_games.write(f'{game["links"][1]["uri"]}\n')
-        existent_link = get_next_page_link(existent_link)
-        print(existent_link)
 
 
+async def fetch(link, offset, session):
+    request = session.get(link.format(offset))
+    return request
 
 
-file = open('list_of_games/list_of_games.txt', 'r')
-if len(file.readlines()) == 0:
-    print('checked')
-    print(datetime.datetime.now())
-    fetching_process('https://www.speedrun.com/api/v1/games?offset=30040')
+async def fetch_ratelimit(semaphore, link, offset, session):
+    async with semaphore:
+        response = await fetch(link, offset, session)
+    return response
 
 
-#runs_list = fetching_process('https://www.speedrun.com/api/v1/games')
-# print(len(runs_list))
+async def get_responses(semaphore, link):
+    async with aiohttp.ClientSession() as session:
+        tasks = [await fetch_ratelimit(semaphore, link, offset, session) for offset in range(0, 30000, 20)]
+        responses = await asyncio.gather(*tasks)
+    return responses
+
+
+url = 'https://www.speedrun.com/api/v1/games?offset={}'
+sem = asyncio.Semaphore(2)
+loop = asyncio.get_event_loop()
+results = loop.run_until_complete(get_responses(sem, url))
+get_jsons(results)
+end = time.time()
+print(end - start)
